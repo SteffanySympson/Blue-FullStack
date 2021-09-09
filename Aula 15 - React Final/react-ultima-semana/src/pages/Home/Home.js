fc2d7396519f261cf9f8fa28ec6fd736726d7922
Steffany Sympson <ssy.marques@gmail.com>
2021-09-08 21:26:20 -0300

    Sass React

---
+++ Aula 15 - React Final/react-ultima-semana/src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Pagina da HOME
+        </div>
+    )
+}
+
+export default Home;
---
+++ Aula 15 - React Final/react-ultima-semana/src/pages/MusicAdd/MusicAdd.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MusicAdd = () => {
+    return (
+        <div>
+            <h1>PAGINA DE CADASTRO DE MUSICA</h1>
+        </div>
+    )
+}
+
+export default MusicAdd;
